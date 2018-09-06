2403c9c2b3e2cafbc27011e9fce10f7116eca7a4
Jul Jen <juliajen.prog@gmail.com>
2018-09-06 19:02:43 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 // use this to copy code snippets or us your browser's console
+
+// - A new User instance is created and assigned to billy, with a name and favorite band assigned as properties in the constructor
 class User {
     constructor(name, favoriteBand) {
         this.name = name;
         this.favoriteBand = favoriteBand;
     }
+
+// favoriteBandMatches is a class function that takes in an array, bands, and filters it to match the favoriteBandproperty previously set
+// 
+// - callback function is declared when favoriteBandMatches method is invoked:
+// - this equals the User instance receiving the method call
+// - bind the callback function to that User instance
+// - From inside the filter method, when the callback function is invoked - the context would be global here, except that the this is already bound to the User instance.
+
     favoriteBandMatches(bands) {
         console.log('in User scope: ', this.favoriteBand);
         return bands.filter(
@@ -14,6 +24,6 @@ class User {
         )[0];
     }
 }
- 
+
 let billy = new User('billy', 'paul simon');
 billy.favoriteBandMatches(['paul simon', 'the kooks']);
